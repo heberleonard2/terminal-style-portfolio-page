dbf615d4fd591e700a8cab28d55af8d9f61c0465
Behnam Ghafary <behnam.ghafary@gmail.com>
2024-03-17 17:50:43 +0100

    customize the content

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Terminal Style Portfolio Page
-
-Site simulando um terminal feito com Javascript, Html e Css.
-#### https://heberleonard2.github.io/terminal-style-portfolio-page/
-
-![](https://user-images.githubusercontent.com/51919658/86963051-477f2400-c13a-11ea-9d34-22885d426c10.gif)
-
-#### Sobre o autor
-###### Héber Leonard
-###### heber.leonard2@gmail.com
-###### Idéias? Fala comigo.
+# behnamgh.github.io
--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,29 @@
-
 const app = document.querySelector("#app");
-const delay = ms => new Promise(res => setTimeout(res, ms));
-    
-    
-app.addEventListener("keypress", async function(event){
-  if(event.key === "Enter"){
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+app.addEventListener("keypress", async function (event) {
+  if (event.key === "Enter") {
     await delay(150);
-   getInputValue();
-   
+    getInputValue();
+
     removeInput();
     await delay(150);
     new_line();
   }
 });
 
-app.addEventListener("click", function(event){
+app.addEventListener("click", function (event) {
   const input = document.querySelector("input");
   input.focus();
-})
+});
 
-
-async function open_terminal(){
+async function open_terminal() {
   createText("Welcome");
   await delay(700);
   createText("Starting the server...");
   await delay(1500);
   createText("You can run several commands:");
- 
+
   createCode("about me", "Who am i and what do i do.");
   createCode("all", "See all commands.");
   createCode("social -a", "All my social networks.");
@@ -35,120 +32,128 @@ async function open_terminal(){
   new_line();
 }
 
-
-function new_line(){
-  
+function new_line() {
   const p = document.createElement("p");
   const span1 = document.createElement("span");
   const span2 = document.createElement("span");
-  p.setAttribute("class", "path")
+  p.setAttribute("class", "path");
   p.textContent = "# user";
   span1.textContent = " in";
-  span2.textContent = " ~/heber-leonard";
+  span2.textContent = " ~/behnam-ghafary";
   p.appendChild(span1);
   p.appendChild(span2);
   app.appendChild(p);
   const div = document.createElement("div");
-  div.setAttribute("class", "type")
+  div.setAttribute("class", "type");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icone");
   const input = document.createElement("input");
   div.appendChild(i);
   div.appendChild(input);
   app.appendChild(div);
   input.focus();
-  
 }
 
-function removeInput(){
+function removeInput() {
   const div = document.querySelector(".type");
   app.removeChild(div);
 }
 
-async function getInputValue(){
-  
+async function getInputValue() {
   const value = document.querySelector("input").value;
-  if(value === "all"){
+  if (value === "all") {
     trueValue(value);
-    
-    createCode("projects", "My github page with my projects. Follow me there ;)");
+
+    createCode(
+      "projects",
+      "My github page with my projects. Follow me there ;)"
+    );
     createCode("about me", "Who am i and what do i do.");
     createCode("social -a", "All my social networks.");
     createCode("clear", "Clean the terminal.");
-    
-  }
-  else if(value === "projects"){
+  } else if (value === "projects") {
     trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
-  }
-  else if(value === "about me"){
+    createText(
+      "<a href='https://github.com/behnamgh' target='_blank'><i class='fab fa-github white'></i> github.com/behnamgh</a>"
+    );
+  } else if (value === "about me") {
     trueValue(value);
-    createText("Oi, meu nome é Héber ;)")
-    createText("Desenvolvedor atualmente focado em todo o ecossistema Javascript. Utilizando principalmente a stack <span class='blue'>Node, React e React Native </span>por permitir criar aplicações de forma descomplicada e produtiva.")
-  }
-  else if(value === "social -a"){
+    createText("Hi, My name is Behnam ;)");
+    createText(
+      "Working as a full-stack developer.I'm currently based in Berlin, Germany. I'm passionate about web development and I love to learn new things. I'm always looking for new opportunities and challenges. My main focus is on entire javascript ecosystem, specially React and Node.js."
+    );
+  } else if (value === "social -a" || value === "social --all") {
     trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
-    createText("<a href='https://www.linkedin.com/in/heber-leonard/' target='_blank'><i class='fab fa-linkedin-in white'></i> linkedin.com/in/heber-leonard</a>")
-    createText("<a href='https://www.instagram.com/heber_leonard/' target='_blank'><i class='fab fa-instagram white'></i> instagram.com/heber_leonard</a>")
-  }
-  else if(value === "social"){
+    createText(
+      "<a href='https://github.com/behnamgh' target='_blank'><i class='fab fa-github white'></i> github.com/behnamgh</a>"
+    );
+    createText(
+      "<a href='https://www.linkedin.com/in/behnamghafary' target='_blank'><i class='fab fa-linkedin-in white'></i> linkedin.com/in/behnamghafary</a>"
+    );
+    createText(
+      "<a href='https://www.instagram.com/behnamghafary/' target='_blank'><i class='fab fa-instagram white'></i> instagram.com/behnamghafary</a>"
+    );
+  } else if (value === "social") {
     trueValue(value);
-    createText("Didn't you mean: social -a?")
-  }
-  
-  else if(value === "clear"){
-    document.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e));
-    document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
-  }
-  else{
+    createText("Didn't you mean: social -a?");
+  } else if (value === "contact") {
+    trueValue(value);
+    createText(
+      "<a href='mailto:behnam.ghafary@gmail.com' target='_blank'><i class='fab fa-envelope white'></i> behnam.ghafary@gmail.com</a>"
+    );
+  } else if (value === "clear") {
+    document.querySelectorAll("p").forEach((e) => e.parentNode.removeChild(e));
+    document
+      .querySelectorAll("section")
+      .forEach((e) => e.parentNode.removeChild(e));
+  } else if (value === "") {
+    trueValue(value);
+  } else if (["mkdir", "ls -la", "ls", "cd ~", "pwd", "ls ~"].includes(value)) {
     falseValue(value);
-    createText(`command not found: ${value}`)
+    createText(`${value}: <span class='error'>Operation not permitted</span>`);
+  } else {
+    falseValue(value);
+    createText(`command not found: ${value}`);
   }
 }
 
-function trueValue(value){
-  
+function trueValue(value) {
   const div = document.createElement("section");
-  div.setAttribute("class", "type2")
+  div.setAttribute("class", "type2");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icone");
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "sucess")
+  mensagem.setAttribute("class", "sucess");
   mensagem.textContent = `${value}`;
   div.appendChild(i);
   div.appendChild(mensagem);
   app.appendChild(div);
 }
 
-function falseValue(value){
-  
+function falseValue(value) {
   const div = document.createElement("section");
-  div.setAttribute("class", "type2")
+  div.setAttribute("class", "type2");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone error")
+  i.setAttribute("class", "fas fa-angle-right icone error");
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "error")
+  mensagem.setAttribute("class", "error");
   mensagem.textContent = `${value}`;
   div.appendChild(i);
   div.appendChild(mensagem);
   app.appendChild(div);
 }
 
-function createText(text, classname){
+function createText(text, classname) {
   const p = document.createElement("p");
-  
-  p.innerHTML =
-  text
-  ;
+
+  p.innerHTML = text;
   app.appendChild(p);
 }
 
-function createCode(code, text){
+function createCode(code, text) {
   const p = document.createElement("p");
   p.setAttribute("class", "code");
-  p.innerHTML =
- `${code} <br/><span class='text'> ${text} </span>`;
+  p.innerHTML = `${code} <br/><span class='text'> ${text} </span>`;
   app.appendChild(p);
 }
 
